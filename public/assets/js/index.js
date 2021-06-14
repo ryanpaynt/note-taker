7022a41c6badfe4c68f9bce75752f694b1ce4f1b
Ryan <ryanpaynt@uncc.edu>
2021-06-14 19:01:29 -0400

    developing routes for note application

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -158,9 +158,9 @@ const renderNoteList = async (notes) => {
     noteListItems.push(createLi('No saved Notes', false));
   }
 
-  jsonNotes.forEach((note) => {
+  jsonNotes.forEach((note, index) => {
     const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
+    li.dataset.note = JSON.stringify(index);
 
     noteListItems.push(li);
   });
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+var notesArr = [];
+
+function updateNotes(notesArr) {
+    notesArr = JSON.stringify(notesArr);
+    fs.writeFileSync("./db/db.json", notesArr);
+}
+
+app.get("/api/notes", function(req, res){
+    notesArr = JSON.parse(fs.readFileSync("./db/db.json", 'utf-8'));
+    return res.json(notesArr);
+});
+
+app.post("/api/notes", function(res, req){
+    notesArr = JSON.parse(fs.readFileSync("./db/db.json", 'utf-8'));
+    var newNote = req.body;
+    notesArr.concat(newNote);
+    updateNotes(notesArr);
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
